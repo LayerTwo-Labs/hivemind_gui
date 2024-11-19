03be0f8e5973a835a2850cf4da578cedce1c4153
CryptAxe <cryptaxe@gmail.com>
2024-11-19 14:28:12 -0800

    Add required pages and page buttons
    Add empty pages for:
        * Overview
        * Wallet
        * Decisions
        * Markets
        * Votes
        * Help info
        * Settings

--- hivemind_gui/script/main_window.gd
+++ hivemind_gui/script/main_window.gd
@@ -1,1 +1,50 @@
 extends Control
+
+
+func _on_button_overview_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/OverviewPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/OverviewPage.visible = false
+
+
+func _on_button_wallet_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/WalletPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/WalletPage.visible = false
+
+
+func _on_button_decisions_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/DecisionPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/DecisionPage.visible = false
+
+
+func _on_button_markets_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/MarketPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/MarketPage.visible = false
+
+
+func _on_button_vote_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/VotePage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/VotePage.visible = false
+
+
+func _on_button_help_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/HelpPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/HelpPage.visible = false
+
+
+func _on_button_settings_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Margin/VBoxMain/HBoxMain/PanelPages/SettingsPage.visible = true
+	else:
+		$Margin/VBoxMain/HBoxMain/PanelPages/SettingsPage.visible = false
